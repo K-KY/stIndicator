34a22386984abe36bb98c22a8181d8006fe0ee35
K-KY <kyuyoungk@naver.com>
2026-03-18 23:14:22 +0900

    feat: application

---
+++ src/test/java/st/indicator/stindicator/StIndicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package st.indicator.stindicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StIndicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
