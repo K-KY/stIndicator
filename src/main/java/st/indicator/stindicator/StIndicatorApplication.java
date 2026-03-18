4e0b226836aa776800b83e4d2d149247da2cc7b6
K-KY <kyuyoungk@naver.com>
2026-03-18 23:14:22 +0900

    feat: application

---
+++ src/main/java/st/indicator/stindicator/StIndicatorApplication.java
@@ -0,0 +1,13 @@
+package st.indicator.stindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StIndicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StIndicatorApplication.class, args);
+    }
+
+}
