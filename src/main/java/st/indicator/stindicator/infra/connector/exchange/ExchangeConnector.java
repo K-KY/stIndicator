19a7ed05cce4a06f4496cd111880ea5d3b511cac
K-KY <kyuyoungk@naver.com>
2026-03-25 21:38:06 +0900

    refactor: ExchangeConnector 추상 메서드 정의

--- src/main/java/st/indicator/stindicator/infra/connector/exchange/ExchangeConnector.java
+++ src/main/java/st/indicator/stindicator/infra/connector/exchange/ExchangeConnector.java
@@ -1,4 +1,21 @@
 package st.indicator.stindicator.infra.connector.exchange;
 
+import com.java.candle.Candle;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
+
 public interface ExchangeConnector {
+    List<Candle> getCandles(Map<String, String> params) throws IOException, NoSuchAlgorithmException,
+            InvalidKeyException, InterruptedException;
+
+    BigDecimal getBalance(Map<String, String> params) throws IOException, NoSuchAlgorithmException,
+            InvalidKeyException, InterruptedException;
+    void orders();
+    void sell();
+    void buy();
 }
