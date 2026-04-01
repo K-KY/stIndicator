ca4f002ce0e6ecaf6050e59b6f889c3a25dcf45e
K-KY <kyuyoungk@naver.com>
2026-04-01 22:37:19 +0900

    feat: 사용자 주문 기록 확인 추상 메서드 추가

--- src/main/java/st/indicator/stindicator/domain/repository/OrderRepository.java
+++ src/main/java/st/indicator/stindicator/domain/repository/OrderRepository.java
@@ -2,7 +2,10 @@ package st.indicator.stindicator.domain.repository;
 
 import st.indicator.stindicator.domain.entity.UserOrder;
 
+import java.util.List;
+
 public interface OrderRepository {
     void saveOrder(UserOrder userOrder);
+    List<UserOrder> getOrders(String symbol);
     void cancelOrder(String orderId);//주문취소
 }
