21d003c235e56e1b0be34b5d021e4a66958eaa21
K-KY <kyuyoungk@naver.com>
2026-04-01 22:37:55 +0900

    feat: symbol 필터로 해당 심볼의 전체 주문 이력 조회 메서드

--- src/main/java/st/indicator/stindicator/infra/connector/repository/OrderJpaRepository.java
+++ src/main/java/st/indicator/stindicator/infra/connector/repository/OrderJpaRepository.java
@@ -2,8 +2,12 @@ package st.indicator.stindicator.infra.connector.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import st.indicator.stindicator.domain.entity.UserOrder;
 import st.indicator.stindicator.infra.connector.entity.UserOrderEntity;
+
+import java.util.List;
 
 @Repository
 public interface OrderJpaRepository extends JpaRepository<UserOrderEntity, Long> {
+    List<UserOrder> findAllBySymbol(String symbol);
 }
