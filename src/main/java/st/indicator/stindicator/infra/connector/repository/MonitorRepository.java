858e057253f436e12f8dcefa17811358ab21be83
K-KY <kyuyoungk@naver.com>
2026-06-07 23:54:20 +0900

    feat: 서버에서 구독한 마켓 데이터가 저장될 리포지토리
    - 바이낸스 api 와 연결은 하나만 유지하고 서버에서 사용자에게 분배하는 형태

--- src/main/java/st/indicator/stindicator/infra/connector/repository/MonitorRepository.java
+++ src/main/java/st/indicator/stindicator/infra/connector/repository/MonitorRepository.java
@@ -5,18 +5,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import st.indicator.stindicator.infra.ws.dto.binance.KlineEventDTO;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class MonitorRepository {
-    private final HashMap<String, KlineEventDTO> lastRepository = new HashMap<>();
+    private final Map<String, KlineEventDTO> lastRepository = new ConcurrentHashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(MonitorRepository.class);
 
-    public void push(KlineEventDTO dto) {
-        lastRepository.put(dto.getSymbol(), dto);
+    public void push(String streamKey, KlineEventDTO dto) {
+        lastRepository.put(streamKey, dto);
     }
 
-    public KlineEventDTO poll(String symbol) {
-        return lastRepository.get(symbol);
+    public KlineEventDTO poll(String streamKey) {
+        return lastRepository.get(streamKey);
     }
 }
