9ca81e46e9be22a59f713708abe887b80a51cad1
K-KY <kyuyoungk@naver.com>
2026-06-15 00:28:37 +0900

    test: 사용하지 않는 테스트 삭제

--- src/test/java/st/indicator/stindicator/infra/connector/exchange/BinanceConnectorTest.java
+++ src/test/java/st/indicator/stindicator/infra/connector/exchange/BinanceConnectorTest.java
@@ -1,13 +1,15 @@
 package st.indicator.stindicator.infra.connector.exchange;
 
-import com.java.candle.Candle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import st.indicator.stindicator.domain.entity.Order;
+import st.indicator.stindicator.domain.utils.candle.Candle;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,6 +36,22 @@ class BinanceConnectorTest {
     void getBalances() throws IOException, NoSuchAlgorithmException, InvalidKeyException, InterruptedException {
         assertThat(binanceConnector.getBalance(Map.of("timestamp", System.currentTimeMillis() + "")))
                 .isNotNull();
+    }
+
+    @Test
+    void order() throws IOException, NoSuchAlgorithmException, InvalidKeyException, InterruptedException {
+        Map<String, String> params = new LinkedHashMap<>();
+        params.put("symbol", "ETHUSDT");//r
+        params.put("side", "BUY");//r
+        params.put("type", "LIMIT");//r
+        params.put("timeInForce", "GTC");
+        params.put("quantity", "1");
+        params.put("price", "1500");
+        params.put("timestamp", System.currentTimeMillis() + "");
+
+        Order order = binanceConnector.order(params);
+        assertThat(order.getOrderId()).isNotNull();
+        System.out.println("order.getOrderId() = " + order.getOrderId());
     }
 
 }
